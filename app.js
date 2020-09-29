12c73be624628fb5f590ad82e9b13a16bd00af15
Steven Penava Jr <steven.penava@gmail.com>
2020-09-29 16:34:26 -0400

    successful stripe integration with tax.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +10,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// var corsOptions = {
+//   origin: '*',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
+
+app.use(cors());
+
+require('dotenv').config({ path: './.env' });
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,13 +36,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.post('/create-checkout-session', async (req, res) => {
+  const domainURL = process.env.DOMAIN;
+
+  const { quantity } = req.body;
+  // Create new Checkout Session for the order
+  // Other optional params include:
+  // [billing_address_collection] - to display billing address details on the page
+  // [customer] - if you have an existing Stripe Customer ID
+  // [customer_email] - lets you prefill the email input in the Checkout page
+  // For full details see https://stripe.com/docs/api/checkout/sessions/create
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: process.env.PAYMENT_METHODS.split(', '),
+    mode: 'payment',
+    line_items: [
+      {
+        price: process.env.PRICE,
+        quantity: quantity,
+        dynamic_tax_rates: ['txr_1HWpTwIZSSMTzx9qIkaJE4LF'],
+      },
+    ],
+    // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+    success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${domainURL}/canceled.html`,
+  });
+
+  res.send({
+    sessionId: session.id,
+  });
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
